00643bd3c67e96bc7fc5273859f1ec60fd98cb63
tczs91 <zhang_s@gwmail.gwu.edu>
2016-06-25 18:53:26 -0400

    Group_1_06_25_submit
    submit on 06/25/2016

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,3 +7,56 @@
  *
  * Main module of the application. Define the routing architecture for the app
  */
+
+ var dashApp = angular.module('dashApp', ['ui.router']);
+
+
+
+ dashApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/login');
+    $stateProvider
+    // INDEX STATES AND NESTED VIEWS ========================================
+        //login view
+        .state('login', {
+            url: '/login',
+            templateUrl: 'templates/login.html',
+            controller: 'loginController'
+        })
+        //root view
+        .state('root', {
+            url: '/root',
+            templateUrl: 'templates/root.html',
+            controller: 'rootController'
+        })
+        //root.work view
+        .state('root.work', {
+            url: '/work',
+            templateUrl: 'templates/work.html',
+            controller: 'workController'
+        })
+        //root.work.delete view
+        .state('root.work.delete', {
+            templateUrl: 'templates/confirmBox.html',
+//            controller:'workController'
+        })
+        //root.overview view
+        .state('root.overview', {
+            url: '/overview',
+            templateUrl: 'templates/overview.html',
+        })
+        //root.producer view
+        .state('root.producer', {
+            url: '/producer',
+            templateUrl: 'templates/producer.html',
+        })
+        //root.contact view
+        .state('root.contact', {
+            url: '/contact',
+            templateUrl: 'templates/contact.html'
+        })
+        //404 not found view
+        .state('notfound', {
+            url: '/notfound',
+            templateUrl: 'templates/404.html'
+        })
+});
--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,3 +2,9 @@
 // LOGIN CONTROLLER
 // Description: Define the following functionalities:
 // Making service calls to login a user
+dashApp.controller('loginController', ['$scope','$state','authenticateService',function($scope, $state, authenticateService) {
+    var message = '';
+    console.log(authenticateService.islogged());
+    $scope.login = authenticateService.login;
+}]);
+
